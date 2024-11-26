76f8907ace1ca9797fd4df48167624a402fd08c1
dileepgoll <167947023+dileepgoll@users.noreply.github.com>
2024-11-26 15:20:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-resource "aws_launch_configuration" "web_server_as" {
+resource "aws_launch_template" "web_server_as" {
+    name = "myproject"
     image_id           = "ami-0327f51db613d7bd2"
     instance_type = "t2.micro"
     key_name = "k8s"
