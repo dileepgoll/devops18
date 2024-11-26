47f9b0aeedaa311b0224dc9f59a9055bcd27cb8c
dileepgoll <167947023+dileepgoll@users.noreply.github.com>
2024-11-26 15:36:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aws_launch_configuration" "web_server_as" {
-
+resource "aws_launch_template" "web_server_as" {
+    name = "myproject"
     image_id           = "ami-0327f51db613d7bd2"
     instance_type = "t2.micro"
     key_name = "k8s"
-  
+    
 }
    
 
@@ -11,7 +11,7 @@ resource "aws_launch_configuration" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-07dd80b215222c0a7", "subnet-055fcb0fea30f6bcf"]
+     subnets = ["subnet-055fcb0fea30f6bcf", "subnet-0e5fb417aeef372ef"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -24,13 +24,15 @@ resource "aws_launch_configuration" "web_server_as" {
   }
 resource "aws_autoscaling_group" "web_server_asg" {
     name                 = "web-server-asg"
-    launch_configuration = aws_launch_configuration.web_server_as.name
     min_size             = 1
     max_size             = 3
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["ap-south-1c", "ap-south-1a"] 
+    availability_zones    = ["ap-south-1a", "ap-south-1b"] 
+    launch_template {
+        id      = aws_launch_template.web_server_as.id
+        version = "$Latest"
+      }
     
   }
-
